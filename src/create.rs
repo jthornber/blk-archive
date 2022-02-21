4380cde9d62778eac498fc3c11190f68aee95ab3
Joe Thornber <ejt@redhat.com>
2022-02-21 12:18:41 +0000

    Add validation for block size, adjust if necc.

--- src/create.rs
+++ src/create.rs
@@ -39,15 +39,36 @@ fn write_config(root: &Path, block_size: usize) -> Result<()> {
     Ok(())
 }
 
+fn adjust_block_size(n: usize) -> usize {
+    // We have a max block size of 1M currently
+    let max_bs = 1024 * 1024;
+    if n > max_bs {
+        return max_bs;
+    }
+
+    let mut p = 1;
+    while p < n {
+        p *= 2;
+    }
+
+    p
+}
+
 pub fn run(matches: &ArgMatches) -> Result<()> {
     let dir = Path::new(matches.value_of("DIR").unwrap());
-    let block_size = matches
+    let mut block_size = matches
         .value_of("BLOCK_SIZE")
         .map(|s| s.parse::<usize>())
         .or(Some(Ok(4096)))
         .unwrap().context("couldn't parse --block-size argument")?;
 
     let report = std::sync::Arc::new(mk_simple_report());
+
+    let new_block_size = adjust_block_size(block_size);
+    if new_block_size != block_size {
+        report.info(&format!("adjusting block size to {}", new_block_size));
+        block_size = new_block_size;
+    }
 
     fs::create_dir(dir)?;
     write_config(dir, block_size)?;
