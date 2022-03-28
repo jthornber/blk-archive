96dfd9ee3562b18ffe294671639a6f4a32da3086
Joe Thornber <ejt@redhat.com>
2022-03-28 08:52:01 +0100

    pull chunkers out to their own file

---
+++ src/chunkers.rs
@@ -0,0 +1,143 @@
+use anyhow::{Result};
+use io::prelude::*;
+use std::fs::{File};
+use std::io;
+use std::ops::Range;
+use std::os::unix::fs::FileExt;
+
+use crate::run_iter::*;
+
+//-----------------------------------------
+
+#[derive(Debug)]
+pub enum Chunk {
+    Mapped(Vec<u8>),
+    Unmapped(u64),
+}
+
+pub struct FileChunker {
+    input: File,
+    input_size: u64,
+    total_read: u64,
+    block_size: usize,
+}
+
+impl FileChunker {
+    pub fn new(input: File, block_size: usize) -> Result<Self> {
+        let input_size = input.metadata()?.len();
+
+        Ok(Self {
+            input,
+            input_size,
+            total_read: 0,
+            block_size,
+        })
+    }
+
+    // FIXME: stop reallocating and zeroing these buffers
+    fn do_read(&mut self, mut buffer: Vec<u8>) -> Result<Option<Chunk>> {
+        self.input.read_exact(&mut buffer)?;
+        self.total_read += buffer.len() as u64;
+        return Ok(Some(Chunk::Mapped(buffer)));
+    }
+
+    fn next_chunk(&mut self) -> Result<Option<Chunk>> {
+        let remaining = self.input_size - self.total_read;
+
+        if remaining == 0 {
+            Ok(None)
+        } else if remaining >= self.block_size as u64 {
+            let buf = vec![0; self.block_size];
+            self.do_read(buf)
+        } else {
+            let buf = vec![0; remaining as usize];
+            self.do_read(buf)
+        }
+    }
+}
+
+impl Iterator for FileChunker {
+    type Item = Result<Chunk>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.next_chunk() {
+            Err(e) => Some(Err(e)),
+            Ok(None) => None,
+            Ok(Some(c)) => Some(Ok(c)),
+        }
+    }
+}
+
+//-----------------------------------------
+
+pub struct ThinChunker<'a> {
+    input: File,
+    run_iter: RunIter<'a>,
+    data_block_size: u64,
+
+    max_read_size: usize,
+    current_run: Option<(bool, Range<u64>)>,
+}
+
+impl<'a> ThinChunker<'a> {
+    pub fn new(input: File, run_iter: RunIter<'a>, data_block_size: u64) -> Self {
+        Self {
+            input,
+            run_iter,
+            data_block_size,
+
+            max_read_size: 16 * 1024 * 1024,
+            current_run: None,
+        }
+    }
+
+    fn next_run_bytes(&mut self) -> Option<(bool, Range<u64>)> {
+        self.run_iter.next().map(|(b, Range { start, end })| {
+            (
+                b,
+                Range {
+                    start: start as u64 * self.data_block_size,
+                    end: end as u64 * self.data_block_size,
+                },
+            )
+        })
+    }
+
+    fn next_chunk(&mut self) -> Result<Option<Chunk>> {
+        let mut run = None;
+        std::mem::swap(&mut run, &mut self.current_run);
+
+        match run.or_else(|| self.next_run_bytes()) {
+            Some((false, run)) => Ok(Some(Chunk::Unmapped(run.end - run.start))),
+            Some((true, run)) => {
+                let run_len = run.end - run.start;
+                if run_len <= self.max_read_size as u64 {
+                    let mut buf = vec![0; run_len as usize];
+                    self.input.read_exact_at(&mut buf, run.start)?;
+                    Ok(Some(Chunk::Mapped(buf)))
+                } else {
+                    let mut buf = vec![0; self.max_read_size];
+                    self.input.read_exact_at(&mut buf, run.start)?;
+                    self.current_run = Some((true, (run.start + buf.len() as u64)..run.end));
+                    Ok(Some(Chunk::Mapped(buf)))
+                }
+            }
+            None => Ok(None),
+        }
+    }
+}
+
+impl<'a> Iterator for ThinChunker<'a> {
+    type Item = Result<Chunk>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mc = self.next_chunk();
+        match mc {
+            Err(e) => Some(Err(e)),
+            Ok(Some(c)) => Some(Ok(c)),
+            Ok(None) => None,
+        }
+    }
+}
+
+//-----------------------------------------
--- src/pack.rs
+++ src/pack.rs
@@ -2,7 +2,6 @@ use anyhow::{anyhow, Context, Result};
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use chrono::prelude::*;
 use clap::ArgMatches;
-use io::prelude::*;
 use io::Write;
 use nom::{bytes::complete::*, multi::*, number::complete::*, IResult};
 use rand::prelude::*;
@@ -10,15 +9,14 @@ use rand_chacha::ChaCha20Rng;
 use size_display::Size;
 use std::collections::BTreeMap;
 use std::env;
-use std::fs::{File, OpenOptions};
+use std::fs::{OpenOptions};
 use std::io;
 use std::io::Cursor;
-use std::ops::Range;
-use std::os::unix::fs::FileExt;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use thinp::report::*;
 
+use crate::chunkers::*;
 use crate::config;
 use crate::content_sensitive_splitter::*;
 use crate::cuckoo_filter::*;
@@ -138,7 +136,7 @@ impl DedupHandler {
 
         // Current slab is always loaded
         if slab == self.current_slab {
-            return Ok(())
+            return Ok(());
         }
 
         match self.slabs_loaded.push(slab) {
@@ -383,139 +381,6 @@ impl IoVecHandler for DedupHandler {
 
 //-----------------------------------------
 
-#[derive(Debug)]
-pub enum Chunk {
-    Mapped(Vec<u8>),
-    Unmapped(u64),
-}
-
-struct FileChunker {
-    input: File,
-    input_size: u64,
-    total_read: u64,
-    block_size: usize,
-}
-
-impl FileChunker {
-    fn new(input: File, block_size: usize) -> Result<Self> {
-        let input_size = input.metadata()?.len();
-
-        Ok(Self {
-            input,
-            input_size,
-            total_read: 0,
-            block_size,
-        })
-    }
-
-    // FIXME: stop reallocating and zeroing these buffers
-    fn do_read(&mut self, mut buffer: Vec<u8>) -> Result<Option<Chunk>> {
-        self.input.read_exact(&mut buffer)?;
-        self.total_read += buffer.len() as u64;
-        return Ok(Some(Chunk::Mapped(buffer)));
-    }
-
-    fn next_chunk(&mut self) -> Result<Option<Chunk>> {
-        let remaining = self.input_size - self.total_read;
-
-        if remaining == 0 {
-            Ok(None)
-        } else if remaining >= self.block_size as u64 {
-            let buf = vec![0; self.block_size];
-            self.do_read(buf)
-        } else {
-            let buf = vec![0; remaining as usize];
-            self.do_read(buf)
-        }
-    }
-}
-
-impl Iterator for FileChunker {
-    type Item = Result<Chunk>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.next_chunk() {
-            Err(e) => Some(Err(e)),
-            Ok(None) => None,
-            Ok(Some(c)) => Some(Ok(c)),
-        }
-    }
-}
-
-//-----------------------------------------
-
-struct ThinChunker<'a> {
-    input: File,
-    run_iter: RunIter<'a>,
-    data_block_size: u64,
-
-    max_read_size: usize,
-    current_run: Option<(bool, Range<u64>)>,
-}
-
-impl<'a> ThinChunker<'a> {
-    fn new(input: File, run_iter: RunIter<'a>, data_block_size: u64) -> Self {
-        Self {
-            input,
-            run_iter,
-            data_block_size,
-
-            max_read_size: 16 * 1024 * 1024,
-            current_run: None,
-        }
-    }
-
-    fn next_run_bytes(&mut self) -> Option<(bool, Range<u64>)> {
-        self.run_iter.next().map(|(b, Range { start, end })| {
-            (
-                b,
-                Range {
-                    start: start as u64 * self.data_block_size,
-                    end: end as u64 * self.data_block_size,
-                },
-            )
-        })
-    }
-
-    fn next_chunk(&mut self) -> Result<Option<Chunk>> {
-        let mut run = None;
-        std::mem::swap(&mut run, &mut self.current_run);
-
-        match run.or_else(|| self.next_run_bytes()) {
-            Some((false, run)) => Ok(Some(Chunk::Unmapped(run.end - run.start))),
-            Some((true, run)) => {
-                let run_len = run.end - run.start;
-                if run_len <= self.max_read_size as u64 {
-                    let mut buf = vec![0; run_len as usize];
-                    self.input.read_exact_at(&mut buf, run.start)?;
-                    Ok(Some(Chunk::Mapped(buf)))
-                } else {
-                    let mut buf = vec![0; self.max_read_size];
-                    self.input.read_exact_at(&mut buf, run.start)?;
-                    self.current_run = Some((true, (run.start + buf.len() as u64)..run.end));
-                    Ok(Some(Chunk::Mapped(buf)))
-                }
-            }
-            None => Ok(None),
-        }
-    }
-}
-
-impl<'a> Iterator for ThinChunker<'a> {
-    type Item = Result<Chunk>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let mc = self.next_chunk();
-        match mc {
-            Err(e) => Some(Err(e)),
-            Ok(Some(c)) => Some(Ok(c)),
-            Ok(None) => None,
-        }
-    }
-}
-
-//-----------------------------------------
-
 // Assumes we've chdir'd to the archive
 fn new_stream_path_(rng: &mut ChaCha20Rng) -> Result<Option<(String, PathBuf)>> {
     // choose a random number
@@ -577,7 +442,8 @@ where
         SlabFile::create(stream_path, 16, true).context("couldn't open stream slab file")?;
 
     let hashes_per_slab = std::cmp::max(SLAB_SIZE_TARGET / block_size, 1);
-    let slab_capacity = ((hash_cache_size_meg * 1024 * 1024) / std::mem::size_of::<Hash256>()) / hashes_per_slab;
+    let slab_capacity =
+        ((hash_cache_size_meg * 1024 * 1024) / std::mem::size_of::<Hash256>()) / hashes_per_slab;
 
     let mut handler = DedupHandler::new(data_file, hashes_file, stream_file, slab_capacity)?;
     handler.ensure_extra_capacity(mapped_size as usize / block_size)?;
