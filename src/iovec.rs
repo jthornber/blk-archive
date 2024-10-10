6daefc88e8b3af0007548f9c007d1c08d74a940c
Tony Asleson <tasleson@redhat.com>
2024-10-10 10:06:40 -0500

    Move IoVec fn(s) to iovec file
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- src/iovec.rs
+++ src/iovec.rs
@@ -17,4 +17,36 @@ pub fn io_vec_to_vec(iov: &IoVec) -> Vec<u8> {
     rc
 }
 
+pub fn iov_len_(iov: &IoVec) -> u64 {
+    let mut len = 0;
+    for v in iov {
+        len += v.len() as u64;
+    }
+
+    len
+}
+
+pub fn first_b_(iov: &IoVec) -> Option<u8> {
+    if let Some(v) = iov.iter().find(|v| !v.is_empty()) {
+        return Some(v[0]);
+    }
+
+    None
+}
+
+pub fn all_same(iov: &IoVec) -> Option<u8> {
+    if let Some(first_b) = first_b_(iov) {
+        for v in iov.iter() {
+            for b in *v {
+                if *b != first_b {
+                    return None;
+                }
+            }
+        }
+        Some(first_b)
+    } else {
+        None
+    }
+}
+
 //-----------------------------------------
