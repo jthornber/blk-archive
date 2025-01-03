3c458694d818534c4a034fa1ad852f5de8c30dff
Benjamin Marzinski <bmarzins@redhat.com>
2025-01-03 17:20:39 -0500

    [splitter] Switch from dynamic to static dispatch
    There's never a point in the code where the splitter methods don't know
    what type of IoVecHandler they are using.

--- src/splitter.rs
+++ src/splitter.rs
@@ -5,11 +5,11 @@ use crate::iovec::*;
 //-----------------------------------------
 
 pub trait Splitter {
-    fn next_data(&mut self, buffer: Vec<u8>, handler: &mut dyn IoVecHandler) -> Result<()>;
+    fn next_data(&mut self, buffer: Vec<u8>, handler: &mut impl IoVecHandler) -> Result<()>;
 
     // Call this to indicate non contiguous data.
-    fn next_break(&mut self, handler: &mut dyn IoVecHandler) -> Result<()>;
-    fn complete(self, handler: &mut dyn IoVecHandler) -> Result<()>;
+    fn next_break(&mut self, handler: &mut impl IoVecHandler) -> Result<()>;
+    fn complete(self, handler: &mut impl IoVecHandler) -> Result<()>;
 }
 
 //-----------------------------------------
