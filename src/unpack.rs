jthornber/blk-archive

commit 06e3b5ae768721f7395fac9a7d083c278f6afa25

Bump clap to latest version

src/unpack.rs
@@ -435,12 +435,12 @@ impl UnpackDest for ThinDest {
 //-----------------------------------------
 
 pub fn run_unpack(matches: &ArgMatches, report: Arc<Report>) -> Result<()> {
-    let archive_dir = Path::new(matches.value_of("ARCHIVE").unwrap())
+    let archive_dir = Path::new(matches.get_one::<String>("ARCHIVE").unwrap())
         .canonicalize()
         .context("Bad archive dir")?;
-    let output_file = Path::new(matches.value_of("OUTPUT").unwrap());
-    let stream = matches.value_of("STREAM").unwrap();
-    let create = matches.is_present("CREATE");
+    let output_file = Path::new(matches.get_one::<String>("OUTPUT").unwrap());
+    let stream = matches.get_one::<String>("STREAM").unwrap();
+    let create = matches.contains_id("CREATE");
 
     let output = if create {
         fs::OpenOptions::new()
@@ -698,9 +698,9 @@ fn thin_verifier(input_file: &Path) -> Result<VerifyDest> {
 }
 
 pub fn run_verify(matches: &ArgMatches, report: Arc<Report>) -> Result<()> {
-    let archive_dir = Path::new(matches.value_of("ARCHIVE").unwrap()).canonicalize()?;
-    let input_file = Path::new(matches.value_of("INPUT").unwrap()).canonicalize()?;
-    let stream = matches.value_of("STREAM").unwrap();
+    let archive_dir = Path::new(matches.get_one::<String>("ARCHIVE").unwrap()).canonicalize()?;
+    let input_file = Path::new(matches.get_one::<String>("INPUT").unwrap()).canonicalize()?;
+    let stream = matches.get_one::<String>("STREAM").unwrap();
 
     env::set_current_dir(archive_dir)?;
 

Cargo.toml
@@ -11,7 +11,7 @@ atty = "0.2"
 blake2 = "0.10"
 byteorder = "1.4"
 chrono = "0.4"
-clap = { version = "3.1.0", features = ["cargo"] }
+clap = { version = "4.5.26", features = ["cargo", "env"] }
 devicemapper = { git = "https://github.com/stratis-storage/devicemapper-rs", branch = "master" }
 gearhash = "0.1.3"
 generic-array = "0.14"
@@ -30,7 +30,7 @@ size-display = "0.1.4"
 thinp = { git = "https://github.com/jthornber/thin-provisioning-tools.git", branch = "main" }
 # thinp = { path = "../thinp-for-dm-archive/" }
 threadpool = "1.0"
-toml = "0.5.8"
+toml = "0.8.19"
 udev = "0.9.1"
 walkdir = "2"
 zstd = "0.13.2"

src/create.rs
@@ -67,15 +67,27 @@ fn adjust_block_size(n: usize) -> usize {
 }
 
 fn numeric_option<T: std::str::FromStr>(matches: &ArgMatches, name: &str, dflt: T) -> Result<T> {
+    match matches.try_get_one::<String>(name) {
+        Ok(Some(s)) => s
+            .parse::<T>()
+            .map_err(|_| anyhow!("could not parse {} argument", name)),
+        Ok(None) => Ok(dflt),
+        Err(_) => Err(anyhow!("Error retrieving {} argument", name)),
+    }
+}
+
+/*
+fn numeric_option<T: std::str::FromStr>(matches: &ArgMatches, name: &str, dflt: T) -> Result<T> {
     matches
         .value_of(name)
         .map(|s| s.parse::<T>())
         .unwrap_or(Ok(dflt))
         .map_err(|_| anyhow!(format!("could not parse {} argument", name)))
 }
+*/
 
 pub fn run(matches: &ArgMatches, report: Arc<Report>) -> Result<()> {
-    let dir = Path::new(matches.value_of("ARCHIVE").unwrap());
+    let dir = Path::new(matches.get_one::<String>("ARCHIVE").unwrap());
 
     let mut block_size = numeric_option::<usize>(matches, "BLOCK_SIZE", 4096)?;
     let new_block_size = adjust_block_size(block_size);

src/dump_stream.rs
@@ -10,8 +10,8 @@ use crate::stream::*;
 //-----------------------------------------
 
 pub fn run(matches: &ArgMatches, output: Arc<Output>) -> Result<()> {
-    let archive_dir = Path::new(matches.value_of("ARCHIVE").unwrap()).canonicalize()?;
-    let stream = matches.value_of("STREAM").unwrap();
+    let archive_dir = Path::new(matches.get_one::<String>("ARCHIVE").unwrap()).canonicalize()?;
+    let stream = matches.get_one::<String>("STREAM").unwrap();
 
     env::set_current_dir(archive_dir)?;
 

src/list.rs
@@ -19,7 +19,7 @@ fn fmt_time(t: &chrono::DateTime<FixedOffset>) -> String {
 }
 
 pub fn run(matches: &ArgMatches, output: Arc<Output>) -> Result<()> {
-    let archive_dir = Path::new(matches.value_of("ARCHIVE").unwrap()).canonicalize()?;
+    let archive_dir = Path::new(matches.get_one::<String>("ARCHIVE").unwrap()).canonicalize()?;
 
     env::set_current_dir(&archive_dir)?;
 

src/main.rs
@@ -15,7 +15,7 @@ use blk_archive::unpack;
 //-----------------------
 
 fn mk_report(matches: &ArgMatches) -> Arc<Report> {
-    if matches.is_present("JSON") {
+    if matches.contains_id("JSON") {
         Arc::new(mk_quiet_report())
     } else if atty::is(atty::Stream::Stdout) {
         Arc::new(mk_progress_bar_report())
@@ -25,28 +25,14 @@ fn mk_report(matches: &ArgMatches) -> Arc<Report> {
 }
 
 fn main_() -> Result<()> {
-    let default_archive = match env::var("BLK_ARCHIVE_DIR") {
-        Err(_) => String::new(),
-        Ok(s) => s,
-    };
-
-    let archive_arg = if default_archive.is_empty() {
-        Arg::new("ARCHIVE")
-            .help("Specify archive directory")
-            .required(true)
-            .long("archive")
-            .short('a')
-            .value_name("ARCHIVE")
-            .takes_value(true)
-    } else {
-        Arg::new("ARCHIVE")
-            .help("Specify archive directory")
-            .default_value(&default_archive)
-            .long("archive")
-            .short('a')
-            .value_name("ARCHIVE")
-            .takes_value(true)
-    };
+    let archive_arg = Arg::new("ARCHIVE")
+        .help("Specify archive directory")
+        .long("archive")
+        .short('a')
+        .value_name("ARCHIVE")
+        .num_args(1)
+        .env("BLK_ARCHIVE_DIR")
+        .required(true);
 
     let stream_arg = Arg::new("STREAM")
         .help("Specify an archived stream to unpack")
@@ -54,7 +40,7 @@ fn main_() -> Result<()> {
         .long("stream")
         .short('s')
         .value_name("STREAM")
-        .takes_value(true);
+        .num_args(1);
 
     let json: Arg = Arg::new("JSON")
         .help("Output JSON")
@@ -62,7 +48,7 @@ fn main_() -> Result<()> {
         .long("json")
         .short('j')
         .value_name("JSON")
-        .takes_value(false);
+        .num_args(1);
 
     let matches = command!()
         .arg(json)
@@ -81,7 +67,7 @@ fn main_() -> Result<()> {
                         .long("archive")
                         .short('a')
                         .value_name("ARCHIVE")
-                        .takes_value(true),
+                        .num_args(1),
                 )
                 .arg(
                     Arg::new("BLOCK_SIZE")
@@ -89,7 +75,7 @@ fn main_() -> Result<()> {
                         .required(false)
                         .long("block-size")
                         .value_name("BLOCK_SIZE")
-                        .takes_value(true),
+                        .num_args(1),
                 )
                 .arg(
                     Arg::new("HASH_CACHE_SIZE_MEG")
@@ -97,7 +83,7 @@ fn main_() -> Result<()> {
                         .required(false)
                         .long("hash-cache-size-meg")
                         .value_name("HASH_CACHE_SIZE_MEG")
-                        .takes_value(true),
+                        .num_args(1),
                 )
                 .arg(
                     Arg::new("DATA_CACHE_SIZE_MEG")
@@ -105,7 +91,7 @@ fn main_() -> Result<()> {
                         .required(false)
                         .long("data-cache-size-meg")
                         .value_name("DATA_CACHE_SIZE_MEG")
-                        .takes_value(true),
+                        .num_args(1),
                 ),
         )
         .subcommand(
@@ -116,7 +102,7 @@ fn main_() -> Result<()> {
                         .help("Specify a device or file to archive")
                         .required(true)
                         .value_name("INPUT")
-                        .takes_value(true),
+                        .num_args(1),
                 )
                 .arg(archive_arg.clone())
                 .arg(
@@ -127,7 +113,7 @@ fn main_() -> Result<()> {
                         .required(false)
                         .long("delta-stream")
                         .value_name("DELTA_STREAM")
-                        .takes_value(true),
+                        .num_args(1),
                 )
                 .arg(
                     Arg::new("DELTA_DEVICE")
@@ -137,7 +123,7 @@ fn main_() -> Result<()> {
                         .required(false)
                         .long("delta-device")
                         .value_name("DELTA_DEVICE")
-                        .takes_value(true),
+                        .num_args(1),
                 ),
         )
         .subcommand(
@@ -148,14 +134,13 @@ fn main_() -> Result<()> {
                         .help("Specify a device or file as the destination")
                         .required(true)
                         .value_name("OUTPUT")
-                        .takes_value(true),
+                        .index(1),
                 )
                 .arg(
                     Arg::new("CREATE")
                         .help("Create a new file rather than unpack to an existing device/file.")
                         .long("create")
-                        .value_name("CREATE")
-                        .takes_value(false),
+                        .action(clap::ArgAction::SetTrue),
                 )
                 .arg(archive_arg.clone())
                 .arg(stream_arg.clone()),
@@ -168,7 +153,7 @@ fn main_() -> Result<()> {
                         .help("Specify a device or file containing the correct version of the data")
                         .required(true)
                         .value_name("INPUT")
-                        .takes_value(true),
+                        .num_args(1),
                 )
                 .arg(archive_arg.clone())
                 .arg(stream_arg.clone()),
@@ -190,7 +175,7 @@ fn main_() -> Result<()> {
     report.set_level(LogLevel::Info);
     let output = Arc::new(Output {
         report: report.clone(),
-        json: matches.is_present("JSON"),
+        json: matches.contains_id("JSON"),
     });
     match matches.subcommand() {
         Some(("create", sub_matches)) => {

src/pack.rs
@@ -699,8 +699,8 @@ fn thin_delta_packer(
 // Looks up both --delta-stream and --delta-device
 fn get_delta_args(matches: &ArgMatches) -> Result<Option<(String, PathBuf)>> {
     match (
-        matches.value_of("DELTA_STREAM"),
-        matches.value_of("DELTA_DEVICE"),
+        matches.get_one::<String>("DELTA_STREAM"),
+        matches.get_one::<String>("DELTA_DEVICE"),
     ) {
         (None, None) => Ok(None),
         (Some(stream), Some(device)) => {
@@ -715,15 +715,15 @@ fn get_delta_args(matches: &ArgMatches) -> Result<Option<(String, PathBuf)>> {
 }
 
 pub fn run(matches: &ArgMatches, output: Arc<Output>) -> Result<()> {
-    let archive_dir = Path::new(matches.value_of("ARCHIVE").unwrap()).canonicalize()?;
-    let input_file = Path::new(matches.value_of("INPUT").unwrap());
+    let archive_dir = Path::new(matches.get_one::<String>("ARCHIVE").unwrap()).canonicalize()?;
+    let input_file = Path::new(matches.get_one::<String>("INPUT").unwrap());
     let input_name = input_file
         .file_name()
         .unwrap()
         .to_str()
         .unwrap()
         .to_string();
-    let input_file = Path::new(matches.value_of("INPUT").unwrap()).canonicalize()?;
+    let input_file = Path::new(matches.get_one::<String>("INPUT").unwrap()).canonicalize()?;
 
     env::set_current_dir(archive_dir)?;
     let config = config::read_config(".")?;

commit e1e92ddee3ba9cc6c1793cc608fb3b32fec60ac4

[unpack] Fix create flag handling.
The create flag handling broke when updating the clap create. When the
SetTrue ArgAction is used, the argument will be either true or false,
but it will always have a value, so contains_id("CREATE") will always be
true.

Fixes: 06e3b5a ("Bump clap to latest version")

src/unpack.rs
@@ -440,7 +440,7 @@ pub fn run_unpack(matches: &ArgMatches, report: Arc<Report>) -> Result<()> {
         .context("Bad archive dir")?;
     let output_file = Path::new(matches.get_one::<String>("OUTPUT").unwrap());
     let stream = matches.get_one::<String>("STREAM").unwrap();
-    let create = matches.contains_id("CREATE");
+    let create = matches.get_flag("CREATE");
 
     let output = if create {
         fs::OpenOptions::new()