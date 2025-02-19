dc2398f5acba3f1b7829dadf9aa0ba041e9ca92e
Tony Asleson <tasleson@redhat.com>
2025-02-19 10:57:15 -0600

    [cli] Add --data-compression=[y|n] option for 'create'
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -92,7 +92,16 @@ fn main_() -> Result<()> {
                         .value_name("HASH_CACHE_SIZE_MEG")
                         .num_args(1),
                 )
-                .arg(data_cache_size.clone()),
+                .arg(data_cache_size.clone())
+                .arg(
+                    Arg::new("DATA_COMPRESSION")
+                        .long("data-compression")
+                        .value_name("y|n")
+                        .help("Enable or disable slab data compression")
+                        .value_parser(["y", "n"]) // Restrict values
+                        .default_value("y")
+                        .action(ArgAction::Set),
+                ),
         )
         .subcommand(
             Command::new("pack")
